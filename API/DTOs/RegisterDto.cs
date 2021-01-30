b27aad835e508eb4d6ee2e87c9ba3c89fa9601e4
Julian Sangillo <juliansangillo@gmail.com>
2021-01-30 18:54:52 -0500

    feat: section 6 - routing

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 namespace API.DTOs {
     public class RegisterDto {
         [Required]
-        public string UserName { get; set; }
+        public string Username { get; set; }
         [Required]
         public string Password { get; set; }
     }
