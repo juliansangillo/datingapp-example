0b124f5757410d3b6ba9202d647337eb2f7f3d3e
Julian Sangillo <juliansangillo@gmail.com>
2021-05-02 16:16:26 -0400

    feat(PhotoManagement): Can now approve or reject photos from ui

--- client/src/app/admin/photo-management/photo-management.component.ts
+++ client/src/app/admin/photo-management/photo-management.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { Photo } from 'src/app/_models/photo';
+import { AdminService } from 'src/app/_services/admin.service';
 
 @Component({
-  selector: 'app-photo-management',
-  templateUrl: './photo-management.component.html',
-  styleUrls: ['./photo-management.component.css']
+    selector: 'app-photo-management',
+    templateUrl: './photo-management.component.html',
+    styleUrls: ['./photo-management.component.css']
 })
 export class PhotoManagementComponent implements OnInit {
+    photos: Photo[];
 
-  constructor() { }
+    constructor(private adminService: AdminService) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.loadPhotos();
+    }
 
+    loadPhotos() {
+        this.adminService.getPhotosToModerate().subscribe(photos => {
+            this.photos = photos;
+        });
+    }
+
+    approvePhoto(id: number) {
+        this.adminService.approvePhoto(id).subscribe(() => {
+            this.loadPhotos();
+        });
+    }
+
+    rejectPhoto(id: number) {
+        this.adminService.rejectPhoto(id).subscribe(() => {
+            this.loadPhotos();
+        });
+    }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -32,6 +32,7 @@ import { UserManagementComponent } from './admin/user-management/user-management
 import { PhotoManagementComponent } from './admin/photo-management/photo-management.component';
 import { RolesModalComponent } from './modals/roles-modal/roles-modal.component';
 import { ConfirmDialogComponent } from './modals/confirm-dialog/confirm-dialog.component';
+import { PhotoApprovalCardComponent } from './admin/photo-approval-card/photo-approval-card.component';
 
 @NgModule({
   declarations: [
@@ -57,7 +58,8 @@ import { ConfirmDialogComponent } from './modals/confirm-dialog/confirm-dialog.c
     UserManagementComponent,
     PhotoManagementComponent,
     RolesModalComponent,
-    ConfirmDialogComponent
+    ConfirmDialogComponent,
+    PhotoApprovalCardComponent
   ],
   imports: [
     BrowserModule,
